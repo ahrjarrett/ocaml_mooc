9bed2458ad35ccd2b6c73303ff7520e0d23f65c1
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-21 13:21:07 -0500

    complete W1 notes and W1 assignment

---
+++ W1/prime_numbers.ml
@@ -0,0 +1,62 @@
+(* Exercise Complete :: 2018-09-21 *)
+(* Andrew Jarrett    :: @ahrjarrett  *)
+
+
+(*
+Let's define some usual arithmetical functions.
+
+1. gcd that takes two non-negative integers n and m, and that returns the greatest common divisor of n and m, following Euclid's algorithm.
+
+2. multiple_upto : int -> int -> bool that takes two non-negative integers n and r, and that tells whether n admits at least one divisor between 2 and r, inclusive. In other words that there exists a number d >= 2 and <= r, such that the remainder of the division of n by d is zero.
+
+3. is_prime a takes a non-negative integer n and checks whether it is a prime number.
+
+Important note: You can assume that both integer_square_root and multiple_of exist, and that they are correct answers to the Simple functions over integers exercise from the previous sequence.
+
+Once is_prime works, you can try writing a new version of it which is self-contained (that contains all definitions of auxiliary functions as locally defined functions).
+ *)
+
+
+let reduce_ints n m = if n > m then (n - m, m) else (n, m - n);;
+let rec gcd n m = 
+  if n = m then n else
+    let (n, m) = reduce_ints n m in
+    gcd n m;;
+
+gcd 252 105;;
+(* : int = 21 *)
+
+
+let rec multiple_upto n r = if r < 2 then false else
+  if n mod r = 0 then true else
+    multiple_upto n (r - 1);;
+
+multiple_upto 123 7;;
+(* : bool = true *)
+multiple_upto 10 7;;
+(* : bool = true *)
+multiple_upto 11 7;;
+(* : bool = false *)
+
+
+let is_prime n = 
+  if n = 1 then false else
+  if n= 2 then true else
+    let i = 2 in
+    let rec prime_test n i = 
+      if i > (n / 2) + 1 then true else
+      if n mod i = 0 then false else
+        prime_test n (i + 1) in
+    prime_test n i;;
+
+is_prime 11;;                   (* : bool = true *)
+is_prime 7;;                    (* : bool = true *)
+is_prime 17;;                   (* : bool = true *)
+
+is_prime 4;;                    (* : bool = false *)
+is_prime 121;;                  (* : bool = false *)
+
+is_prime 1;;                    (* : bool = false *)
+is_prime 2;;                    (* : bool = true *)
+is_prime 3;;                    (* : bool = true *)
+
